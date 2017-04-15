a21e5b74cc96689ee7e93db544196bbf1965113e
JayZejianZhou <zzhou12@stevens.edu>
2017-04-15 15:11:44 -0400

    I'm gonna fuck this project with V-REP, yeah, bitch!

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zejian/final_proj_short_cut/src
+# Build directory: /home/zejian/final_proj_short_cut/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(final_test)
---
+++ build/final_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zejian/final_proj_short_cut/src/final_test
+# Build directory: /home/zejian/final_proj_short_cut/build/final_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
