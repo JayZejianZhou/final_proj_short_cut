e61957858b60d88a628fb9f8e033035c5dd43591
JayZejianZhou <zzhou12@stevens.edu>
2017-04-19 11:23:17 -0400

    before 1st demonstration

--- src/final_test/src/ped_marker.cpp
+++ src/final_test/src/ped_marker.cpp
@@ -10,159 +10,38 @@
 #include <pedsim/ped_scene.h>
 
 void marker_initiate(std::vector<visualization_msgs::Marker> &paths){
-//This function is used to generate path markers in rviz
-
-  //path 0-------0 based
-  visualization_msgs::Marker path;
-  path.header.frame_id="/map";
-  path.header.stamp=ros::Time::now();
-  path.ns="path0";
-  path.id=0;// 0---based
-  path.action=visualization_msgs::Marker::ADD;
-  path.type=visualization_msgs::Marker::LINE_STRIP;
-  path.scale.x=0.1;
-  path.color.r=0.9f;
-  path.color.g=1.0f;
-  path.color.b=0.9f;
-  path.color.a=1.0f;
-  paths.push_back(path);
-
-  //path 1-------0 based
-  visualization_msgs::Marker path1;
-  path1.header.frame_id="/map";
-  path1.header.stamp=ros::Time::now();
-  path1.ns="path1";
-  path1.id=1;// 0---based
-  path1.action=visualization_msgs::Marker::ADD;
-  path1.type=visualization_msgs::Marker::LINE_STRIP;
-  path1.scale.x=0.1;
-  path1.color.r=0.0f;
-  path1.color.g=1.0f;
-  path1.color.b=0.0f;
-  path1.color.a=1.0f;
-  paths.push_back(path1);
-
-  //path 1-------1 based
-  visualization_msgs::Marker path2;
-  path2.header.frame_id="/map";
-  path2.header.stamp=ros::Time::now();
-  path2.ns="path2";
-  path2.id=2;// 0---based
-  path2.action=visualization_msgs::Marker::ADD;
-  path2.type=visualization_msgs::Marker::LINE_STRIP;
-  path2.scale.x=0.1;
-  path2.color.r=0.8f;
-  path2.color.g=1.0f;
-  path2.color.b=0.8f;
-  path2.color.a=1.0f;
-  paths.push_back(path2);
-
-  //path 1-------1 based
-  visualization_msgs::Marker path3;
-  path3.header.frame_id="/map";
-  path3.header.stamp=ros::Time::now();
-  path3.ns="path3";
-  path3.id=3;// 0---based
-  path3.action=visualization_msgs::Marker::ADD;
-  path3.type=visualization_msgs::Marker::LINE_STRIP;
-  path3.scale.x=0.1;
-  path3.color.r=0.7f;
-  path3.color.g=1.0f;
-  path3.color.b=0.7f;
-  path3.color.a=1.0f;
-  paths.push_back(path3);
-
-  //path 1-------1 based
-  visualization_msgs::Marker path4;
-  path4.header.frame_id="/map";
-  path4.header.stamp=ros::Time::now();
-  path4.ns="path4";
-  path4.id=4;// 0---based
-  path4.action=visualization_msgs::Marker::ADD;
-  path4.type=visualization_msgs::Marker::LINE_STRIP;
-  path4.scale.x=0.1;
-  path4.color.r=0.6f;
-  path4.color.g=1.0f;
-  path4.color.b=0.6f;
-  path4.color.a=1.0f;
-  paths.push_back(path4);
-
-  //path 5-------0 based
-  visualization_msgs::Marker path5;
-  path5.header.frame_id="/map";
-  path5.header.stamp=ros::Time::now();
-  path5.ns="path5";
-  path5.id=5;// 0---based
-  path5.action=visualization_msgs::Marker::ADD;
-  path5.type=visualization_msgs::Marker::LINE_STRIP;
-  path5.scale.x=0.1;
-  path5.color.r=0.5f;
-  path5.color.g=1.0f;
-  path5.color.b=0.5f;
-  path5.color.a=1.0f;
-  paths.push_back(path5);
-
-  //path 6-------0 based
-  visualization_msgs::Marker path6;
-  path6.header.frame_id="/map";
-  path6.header.stamp=ros::Time::now();
-  path6.ns="path6";
-  path6.id=6;// 0---based
-  path6.action=visualization_msgs::Marker::ADD;
-  path6.type=visualization_msgs::Marker::LINE_STRIP;
-  path6.scale.x=0.1;
-  path6.color.r=0.4f;
-  path6.color.g=1.0f;
-  path6.color.b=0.4f;
-  path6.color.a=1.0f;
-  paths.push_back(path6);
-
-  //path 7-------0 based
-  visualization_msgs::Marker path7;
-  path7.header.frame_id="/map";
-  path7.header.stamp=ros::Time::now();
-  path7.ns="path7";
-  path7.id=7;// 0---based
-  path7.action=visualization_msgs::Marker::ADD;
-  path7.type=visualization_msgs::Marker::LINE_STRIP;
-  path7.scale.x=0.1;
-  path7.color.r=0.3f;
-  path7.color.g=1.0f;
-  path7.color.b=0.3f;
-  path7.color.a=1.0f;
-  paths.push_back(path7);
-
-  //path 8-------0 based
-  visualization_msgs::Marker path8;
-  path8.header.frame_id="/map";
-  path8.header.stamp=ros::Time::now();
-  path8.ns="path8";
-  path8.id=8;// 0---based
-  path8.action=visualization_msgs::Marker::ADD;
-  path8.type=visualization_msgs::Marker::LINE_STRIP;
-  path8.scale.x=0.1;
-  path8.color.r=0.2f;
-  path8.color.g=1.0f;
-  path8.color.b=0.2f;
-  path8.color.a=1.0f;
-  paths.push_back(path8);
-
-  //path 9-------0 based
-  visualization_msgs::Marker path9;
-  path9.header.frame_id="/map";
-  path9.header.stamp=ros::Time::now();
-  path9.ns="path9";
-  path9.id=9;// 0---based
-  path9.action=visualization_msgs::Marker::ADD;
-  path9.type=visualization_msgs::Marker::LINE_STRIP;
-  path9.scale.x=0.1;
-  path9.color.r=0.1f;
-  path9.color.g=1.0f;
-  path9.color.b=0.1f;
-  path9.color.a=1.0f;
-  paths.push_back(path9);
-
-
+//generate path markers in rviz
+  for(int i=0;i<10;i++){
+    visualization_msgs::Marker path;
+    path.header.frame_id="/map";
+    path.header.stamp=ros::Time::now();
+    path.ns="path";
+    path.id=i;// 0---based
+    path.action=visualization_msgs::Marker::ADD;
+    path.type=visualization_msgs::Marker::LINE_STRIP;
+    path.scale.x=0.1;
+    path.color.r=i/10;
+    path.color.g=1;
+    path.color.b=i/10;
+    path.color.a=1.0f;
+    paths.push_back(path);
+  }
+////generate scene boundries markers
+//  for(int i=0;i<4;i++){
+//    visualization_msgs::Marker scene;
+//    scene.header.frame_id="/map";
+//    scene.header.stamp=ros::Time::now();
+//    scene.ns="path";
+//    scene.id=i;// 0---based
+//    scene.action=visualization_msgs::Marker::ADD;
+//    scene.type=visualization_msgs::Marker::LINE_STRIP;
+//    scene.scale.x=0.5;
+//    scene.color.r=1;
+//    scene.color.g=0;
+//    scene.color.b=0;
+//    scene.color.a=1.0f;
+//    scenes.push_back(scene);
+//  }
 }
 
 //this function draw paths for every agent
--- src/final_test/src/pedsim_test.cpp
+++ src/final_test/src/pedsim_test.cpp
@@ -1,7 +1,4 @@
 #include <ros/ros.h>
-//#include <libpedsim/ped_includes.h>
-//#include <libpedsim/ped_outputwriter.h>
-//#include <libpedsim/ped_scene.h>
 #include <pedsim/ped_scene.h>
 #include <pedsim/ped_waypoint.h>
 #include <pedsim/ped_outputwriter.h>
@@ -17,72 +14,147 @@
 
 
 std::vector<visualization_msgs::Marker> paths;
+//std::vector<visualization_msgs::Marker> scene_marker;
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pedsim_demo");
   ros::NodeHandle nh("~");
   ros::Publisher path_pub=nh.advertise<visualization_msgs::Marker>("path",20);
-  ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
-  ros::Rate r(1);
+//  ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+  ros::Publisher scene_pub=nh.advertise<visualization_msgs::Marker>("scene",20);
+  ros::Rate r(5);
+  while(scene_pub.getNumSubscribers()<1){
+    if(!ros::ok())  return 0;
+    ROS_WARN_ONCE("Please create a subscriber to the maker");
+    sleep(1);
+  }
   // initiate markers
   marker_initiate(paths);
 
-  //create grid map
-  grid_map::GridMap map({"elevation"});
-  map.setFrameId("map");
-  map.setGeometry(grid_map::Length(3.0,3.0),0.03);
-
-  //-----------------this is a simple test---------------------
+  //----------------Pedsim initiate---------------------
   //create output writer
   Ped::OutputWriter *ow = new Ped::FileOutputWriter();
   ow->setScenarioName("Example_self 01");
 
-  Ped::Tscene *pedscene = new Ped::Tscene(-10,-10,40,40);
+  //scene parameter process
+  int scene_data[4];//left, top, width, height
+  int waypoint[4];//x1, y1, r1,x2, y2, r2
+  nh.param<int>("scene_left",scene_data[0],-200);
+  nh.param<int>("scene_top",scene_data[1],-200);
+  nh.param<int>("scene_width",scene_data[2],-200);
+  nh.param<int>("scene_height",scene_data[3],-200);
+
+  nh.param<int>("w1_x",waypoint[0],-200);
+  nh.param<int>("w1_y",waypoint[1],-200);
+  nh.param<int>("w1_r",waypoint[2],-200);
+  nh.param<int>("w2_x",waypoint[3],-200);
+  nh.param<int>("w2_y",waypoint[4],-200);
+  nh.param<int>("w2_r",waypoint[5],-200);
+
+//  //draw the boundry
+//  geometry_msgs::Point left_down;
+//  left_down.x=scene_data[0];
+//  left_down.y=scene_data[1]-scene_data[3];
+//  left_down.z=0;
+//  geometry_msgs::Point left_up;
+//  left_down.x=scene_data[0];
+//  left_down.y=scene_data[1];
+//  left_down.z=0;
+//  geometry_msgs::Point right_down;
+//  left_down.x=scene_data[0]+scene_data[2];
+//  left_down.y=scene_data[1]-scene_data[3];
+//  left_down.z=0;
+//  geometry_msgs::Point  right_up;
+//  left_down.x=scene_data[0]+scene_data[2];
+//  left_down.y=scene_data[1];
+//  left_down.z=0;
+//  scene_marker[0].points.push_back(left_down);
+//  scene_marker[0].points.push_back(left_up);
+//  scene_marker[1].points.push_back(right_up);
+//  scene_marker[1].points.push_back(right_down);
+//  scene_marker[2].points.push_back(left_down);
+//  scene_marker[2].points.push_back(right_down);
+//  scene_marker[3].points.push_back(left_up);
+//  scene_marker[3].points.push_back(right_up);
+
+//  for(std::vector<visualization_msgs::Marker>::iterator it=scene_marker.begin();it!=scene_marker.end();++it)
+//    scene_pub.publish(*it);
+
+//set the scene
+  Ped::Tscene *pedscene = new Ped::Tscene(scene_data[0],scene_data[1],scene_data[2],scene_data[3]);
   pedscene->setOutputWriter(ow);
 
-   Ped::Twaypoint *w1 =  new Ped::Twaypoint(0,0,0);
-   Ped::Twaypoint *w2 =  new Ped::Twaypoint(10,10,12);
+   Ped::Twaypoint *w1 =  new Ped::Twaypoint(waypoint[0],waypoint[1],waypoint[2]);
+   Ped::Twaypoint *w2 =  new Ped::Twaypoint(waypoint[3],waypoint[4],waypoint[5]);
 
- // Ped::Tobstacle *o = new Ped::Tobstacle(0,-50,0,+50);
- // pedscene->addObstacle(o);
+  Ped::Tobstacle *o = new Ped::Tobstacle(-50,0,-20,4);
+  pedscene->addObstacle(o);
   int pos=0;
   for (int i=0;i<10;i++){
     Ped::Tagent *a =new Ped::Tagent();
-  //  a->addWaypoint(w1);
+    a->addWaypoint(w1);
     a->addWaypoint(w2);
 
     a->setPosition(0,pos++,0);
 
     pedscene->addAgent(a);
   }
+  //set obstacle
+  visualization_msgs::Marker obs;
+  obs.header.frame_id="/map";
+  obs.header.stamp=ros::Time::now();
+  obs.ns="obstacle";
+  obs.id=0;// 0---based
+  obs.action=visualization_msgs::Marker::ADD;
+  obs.type=visualization_msgs::Marker::CUBE;
+  obs.pose.position.x=-35;
+  obs.pose.position.y=2;
+  obs.pose.position.z=0;
+  obs.pose.orientation.x=0;
+  obs.pose.orientation.y=0;
+  obs.pose.orientation.z=0;
+  obs.pose.orientation.w=0;
+  obs.scale.x=15.0;
+  obs.scale.y=2.0;
+  obs.scale.z=4.0;
+  obs.color.r=1.0f;
+  obs.color.g=0.0f;
+  obs.color.b=0.0f;
+  obs.color.a=1.0;
+  obs.lifetime=ros::Duration();
+  scene_pub.publish(obs);
 
-  //give the map a value
-  for (grid_map::GridMapIterator it(map); !it.isPastEnd(); ++it) {
-     grid_map::Position position;
-     map.getPosition(*it, position);
-     map.at("elevation", *it) = 0;
-//      map.at("elevation", *it) =2;
-   }
+//  //create grid map
+//  grid_map::GridMap map({"elevation"});
+//  map.setFrameId("map");
+//  map.setGeometry(grid_map::Length(20,20),1);
 
 
+//  //give the map a value
+//  for (grid_map::GridMapIterator it(map); !it.isPastEnd(); ++it) {
+//     grid_map::Position position;
+//     map.getPosition(*it, position);
+//     map.at("elevation", *it) = 0;
+////      map.at("elevation", *it) =2;
+//   }
+
+//  //publish the grid map
+//  ros::Time time = ros::Time::now();
+//  map.setTimestamp(time.toNSec());
+//  grid_map_msgs::GridMap message;
+//  grid_map::GridMapRosConverter::toMessage(map, message);
+//  publisher.publish(message);
 
   while(nh.ok()){
-    ros::Time time = ros::Time::now();
 
 
-
-      pedscene->moveAgents(1.0);
+      pedscene->moveAgents(0.5);
       draw_path(paths,pedscene);
       for(std::vector<visualization_msgs::Marker>::iterator it=paths.begin();it!=paths.end();++it)
         path_pub.publish(*it);
 
 
-      //publish the grid map
-      map.setTimestamp(time.toNSec());
-      grid_map_msgs::GridMap message;
-      grid_map::GridMapRosConverter::toMessage(map, message);
-      publisher.publish(message);
       r.sleep();
 
   }
